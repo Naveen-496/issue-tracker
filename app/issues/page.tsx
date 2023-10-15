271c2596960aba9d41cb7e3623095494a5267179
naveen.reddy <naveen.reddy@dheeyantra.com>
2023-10-15 20:32:41 +0530

    setup radix ui

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,8 +1,7 @@
-import React from 'react'
-
+import { Button } from "@radix-ui/themes";
 const IssuesPage = () => {
   return (
-    <div>IssuesPage</div>
+    <div><Button>New Issue</Button></div>
   )
 }
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Navbar from './Navbar'
+import './globals.css';
+import '@radix-ui/themes/styles.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import Navbar from './Navbar';
+import { Theme } from '@radix-ui/themes';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,10 +20,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Theme>
         <Navbar />
         <main>
           {children}
         </main>
+        </Theme>
         </body>
     </html>
   )
