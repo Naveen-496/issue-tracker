535b6700ac46a7c606d32498e04fe073ea00ceb3
naveen.reddy <naveen.reddy@dheeyantra.com>
2023-10-20 10:12:36 +0530

    refactor assignee select component

--- app/issues/[id]/SelectAssignee.tsx
+++ app/issues/[id]/SelectAssignee.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import { Toaster, toast } from "sonner";
 
 {
   /* 
@@ -26,22 +27,19 @@ const SelectAssignee = ({ issue }: { issue: Issue }) => {
     data: users,
     error,
     isLoading,
-  } = useQuery<User[]>({
-    queryKey: ["users"],
-    queryFn: () => axios.get("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000, // 60 secs
-    retry: 3,
-  });
+  } = useUsers();
 
   if (isLoading) return <Skeleton />;
 
   if (error) return null;
 
   const handleSelectChange = async (userId: string) => {
-    axios.patch("/api/issues/" + issue.id, { assignedToUserId: userId === "unassigned" ? null: userId});
+    axios.patch("/api/issues/" + issue.id, { assignedToUserId: userId === "unassigned" ? null: userId}).catch( err => toast.error( err.message));
   };
 
   return (
+    <>
+    <Toaster richColors />
     <Select.Root 
     defaultValue={ issue.assignedToUserId || "unassigned"}
     onValueChange={(userId) => handleSelectChange(userId)}
@@ -59,7 +57,15 @@ const SelectAssignee = ({ issue }: { issue: Issue }) => {
         </Select.Group>
       </Select.Content>
     </Select.Root>
+    </>
   );
 };
+
+const useUsers = () => useQuery<User[]>({
+  queryKey: ["users"],
+  queryFn: () => axios.get("/api/users").then((res) => res.data),
+  staleTime: 60 * 1000, // 60 secs
+  retry: 3,
+});
 
 export default SelectAssignee;
